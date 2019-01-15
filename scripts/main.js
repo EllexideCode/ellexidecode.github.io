2fb2f3fa8fe675a3a2cbf72308c3d7a75f6987d6
Ellexide <ellexidecodes@gmail.com>
2019-01-15 22:26:12 +0800

    log it

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,8 @@ $(document).ready(()=>{
   function hello() {
     $.getJSON("./scripts/profiles.json", function(result) {
       $.each(result, function(field) {
-        $("body").append(field.name + " " + field.preview + "<br>");
+        console.log(result);
+        console.log(field);
       })
     })
   };
