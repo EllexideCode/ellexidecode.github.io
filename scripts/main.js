88efc0b3568c4f7c43d8b6633ba0c8ad164d14a7
Ellexide <ellexidecodes@gmail.com>
2019-01-15 22:30:34 +0800

    test again

--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,12 @@ $(document).ready(()=>{
   function hello() {
     $.getJSON("./scripts/profiles.json", function(result) {
       $.each(result, function(field) {
-        console.log(result);
-        console.log(field);
+        var size = field.length;
+        for(i = 0; i < size; ++i) {
+          $("body").append(`
+          This is a ${field}. The name of the profile owner is ${field[i].name}. You can find their profile here: ${field[i].preview}. Please work.
+          `)
+        }
       })
     })
   };
